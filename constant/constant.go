2104a2f1daf638233e8410c754203efd55626ee2
anhduc1603 <saotrucanhduc@gmail.com>
2025-03-09 23:52:20 +0700

    git commit

--- constant/constant.go
+++ constant/constant.go
@@ -1,6 +1,3 @@
 package constant
 
-const (
-	ClientID     = "470576649745-cls4dp37n5ia2li1ddnaktqq4v9ijk6f.apps.googleusercontent.com"
-	ClientSecret = "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"
-)
+const ()
